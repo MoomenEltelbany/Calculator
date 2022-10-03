a4c5faffeb096028d46c09ff64a510db50ad1fe2
Moomen Eltelbany <73857848+MoomenEltelbany@users.noreply.github.com>
2022-10-03 16:26:41 -0400

    Initial commit

---
+++ main.js
@@ -0,0 +1,32 @@
+let display = document.querySelector(".display");
+
+let allBtns = document.querySelectorAll("button");
+
+const allBtnsArray = Array.from(allBtns);
+
+allBtnsArray.forEach((ele) => {
+    ele.addEventListener("click", (e) => {
+        try {
+            if (e.target.innerText === "AC") {
+                display.innerText = "";
+            } else if (e.target.innerText === "DEL") {
+                display.innerText = display.innerText.slice(0, -1);
+            } else if (e.target.innerText === "=") {
+                display.innerText = eval(display.innerText);
+            } else {
+                display.innerText += e.target.innerText;
+            }
+        } catch {
+            display.innerText = `ERROR`;
+        }
+    });
+});
+
+let changeBtn = document.querySelector(".change");
+let controller = document.querySelector(".controller");
+
+console.log(changeBtn);
+console.log(controller);
+changeBtn.onclick = () => {
+    controller.classList.toggle("dark");
+};
